4fbc74e760984c3498fbd005d1c2a5db770dcd70
Fabian Morón Zirfas <icke@fabianmoronzirfas.me>
2019-05-14 11:43:32 +0200

    fix(deploy to netlify):

--- store/index.js
+++ store/index.js
@@ -10,10 +10,10 @@ import toUrl from '~/assets/js/tourl.js'
 
 //adapt this before you generate the site
 // const productionUrl = "http://hanshack.com/tsb";
-const productionUrl = (process.env.NODE_ENV === 'development') ? "http://localhost:3000" : "http://bezirksregionenprofile-env.w42dwkkrhe.eu-central-1.elasticbeanstalk.com";
+const productionUrl = (process.env.NODE_ENV === "production") ? "https://bezirksregionenprofile.netlify.com" : "http://localhost:3000";
 
 //define the source of the tiles etc
-bright.sources= {
+bright.sources = {
   "openmaptiles": {
     "attribution": "<a href='http://openmaptiles.org'>© OpenMapTiles</a> <a href='http://www.openstreetmap.org/copyright'>© OpenStreetMap</a>",
     "type": "vector",
@@ -21,8 +21,8 @@ bright.sources= {
     // "tiles": [productionUrl + "/map/tiles/{z}/{x}/{y}.pbf"]
   }
 };
-bright.sprite= productionUrl + "/map/sprites/positron";
-bright.glyphs= productionUrl + "/map/glyphs/{fontstack}/{range}.pbf";
+bright.sprite = productionUrl + "/map/sprites/positron";
+bright.glyphs = productionUrl + "/map/glyphs/{fontstack}/{range}.pbf";
 
 //no source here
 nostyle.sources = {};
@@ -39,7 +39,7 @@ for (var i = 0; i < bezirksgrenzen.features.length; i++) {
   bezirksNamen[name].url = toUrl(name);
 }
 const bezirksNamenOrdered = {};
-Object.keys(bezirksNamen).sort().forEach(function(key) {
+Object.keys(bezirksNamen).sort().forEach(function (key) {
   bezirksNamenOrdered[key] = bezirksNamen[key];
 });
 
@@ -54,7 +54,7 @@ for (var i = 0; i < bezirksregionen.features.length; i++) {
   bezirksregionenNamen[name].bzName = bzName;
 }
 const bezirksregionenNamenOrdered = {};
-Object.keys(bezirksregionenNamen).sort().forEach(function(key) {
+Object.keys(bezirksregionenNamen).sort().forEach(function (key) {
   bezirksregionenNamenOrdered[key] = bezirksregionenNamen[key];
 });
 
@@ -72,20 +72,20 @@ const planungsraeume = topojson.feature(bzr_lor, bzr_lor.objects["lor_planungsra
 // }
 
 const themen = [
-  {"name": "Kurzporträt der Bezirksregion","id":1},
-  {"name": "Demografische Struktur und Entwicklung","id":2},
-  {"name": "Soziale Situation","id":3},
-  {"name": "Bildungssituation","id":4},
-  {"name": "Gesundheitliche Situation","id":5},
-  {"name": "Wirtschafts- und Gewerbestruktur","id":6},
-  {"name": "Wohnsituation","id":7},
-  {"name": "Soziale Infrastruktur","id":8},
-  {"name": "Grüne Infrastruktur und öffentlicher Raum","id":9},
-  {"name": "Mobilität","id":10},
-  {"name": "Umwelt (Lärmbelastung / Luftbelastung / Bioklima)","id":11},
-  {"name": "Förderprogramme und Projekte","id":12},
-  {"name": "Partizipation: Strukturen der Akteursvernetzung und Bürgerbeteiligung","id":13},
-  {"name": "Zusammenfassung: Potenziale und Herausforderungen","id":14},
+  { "name": "Kurzporträt der Bezirksregion", "id": 1 },
+  { "name": "Demografische Struktur und Entwicklung", "id": 2 },
+  { "name": "Soziale Situation", "id": 3 },
+  { "name": "Bildungssituation", "id": 4 },
+  { "name": "Gesundheitliche Situation", "id": 5 },
+  { "name": "Wirtschafts- und Gewerbestruktur", "id": 6 },
+  { "name": "Wohnsituation", "id": 7 },
+  { "name": "Soziale Infrastruktur", "id": 8 },
+  { "name": "Grüne Infrastruktur und öffentlicher Raum", "id": 9 },
+  { "name": "Mobilität", "id": 10 },
+  { "name": "Umwelt (Lärmbelastung / Luftbelastung / Bioklima)", "id": 11 },
+  { "name": "Förderprogramme und Projekte", "id": 12 },
+  { "name": "Partizipation: Strukturen der Akteursvernetzung und Bürgerbeteiligung", "id": 13 },
+  { "name": "Zusammenfassung: Potenziale und Herausforderungen", "id": 14 },
 ]
 
 Vue.use(Vuex)
@@ -94,16 +94,16 @@ const store = () => new Vuex.Store({
   state: {
     bezirksgrenzen: bezirksgrenzen,
     bezirksregionen: bezirksregionen,
-    planungsraeume:planungsraeume,
-    bzNamen:bezirksNamenOrdered,
+    planungsraeume: planungsraeume,
+    bzNamen: bezirksNamenOrdered,
     bzrNamen: bezirksregionenNamenOrdered,
     brightstyle: bright,
     nostyle: nostyle,
-    mapColors: ["#1E3791","#E60032"],
-    indikatorenOverview:indikatorenOverview,
-    themen:themen,
-    urldev:'http://localhost:3000',
-    url:  productionUrl
+    mapColors: ["#1E3791", "#E60032"],
+    indikatorenOverview: indikatorenOverview,
+    themen: themen,
+    urldev: 'http://localhost:3000',
+    url: productionUrl
   },
   getters: {
     // toUrl(str){
